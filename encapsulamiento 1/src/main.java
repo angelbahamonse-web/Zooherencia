5dbbcdf7d5412269fcdb81f36db9e223eb68fc3b
Angel Felipe <angel_bahamonse@fet.edu.co>
2025-09-12 19:49:26 -0500

    Delete encapsulamiento 1 directory

--- encapsulamiento 1/src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        CuentaBancaria cuenta = new CuentaBancaria();
-        cuenta.depositar(500);
-        cuenta.retirar(200);
-        System.out.println("Saldo: " + cuenta.getSaldo()); // debe imprimir 300
-        }
-}
